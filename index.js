6e117f4af0fa285c427fdb9adadd5d11c79c232e
Kevin Aleman <kevin.aleman@utexas.edu>
2016-12-02 21:35:39 -0800

    omg.

--- index.js
+++ index.js
@@ -4,11 +4,6 @@ var app = express();
 app.use(express.static(__dirname + '/public'));
 app.set('port', (process.env.PORT));
 
-app.configure(function(){
-  app.use(express.bodyParser());
-  app.use(app.router);
-});
-
 app.listen(process.env.PORT);
 
 require('./app/bot')(app);
