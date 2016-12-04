84b2bdebf103e1d16b580c20bf3916e140185ee8
Kevin Aleman <kevin.aleman@utexas.edu>
2016-12-03 22:15:38 -0800

    It works!

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('port', (process.env.PORT));
 
 app.listen(process.env.PORT);
