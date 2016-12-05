666517554ea312a34e28afa06fbea4f02f1a7533
Kevin Aleman <kevin.aleman@utexas.edu>
2016-12-04 17:17:46 -0800

    Test getting the SSL cert challenge response.

--- server.js
+++ server.js
@@ -7,6 +7,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('port', (process.env.PORT));
 
+const letsEncryptReponse = process.env.CERTBOT_RESPONSE;
+
+// Return the Let's Encrypt certbot response:
+app.get('/.well-known/acme-challenge/:content', function(req, res) {
+  res.send(letsEncryptReponse);
+});
+
 require('./app/bot')(app);
 
 app.listen(process.env.PORT);
