5ed325321f689e848f49ede43a35cf325dd38412
Kevin Aleman <kevin.aleman@utexas.edu>
2016-12-04 17:59:05 -0800

    No longer need the acme challenge endpoint.

--- server.js
+++ server.js
@@ -7,13 +7,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('port', (process.env.PORT));
 
-const letsEncryptReponse = process.env.CERTBOT_RESPONSE;
-
-// Return the Let's Encrypt certbot response:
-app.get('/.well-known/acme-challenge/:content', function(req, res) {
-  res.send(letsEncryptReponse);
-});
-
 require('./app/bot')(app);
 
 app.listen(process.env.PORT);
